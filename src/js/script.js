b4cd8a01135e048b8643a9043948722f18bc28bf
bastien17-dev <bastien.paquier@gmail.com>
2020-05-20 15:56:32 +0200

    add bootstrap in the project"

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+fetch('https://api.yelp.com/v3/businesses/search?location=paris&term=barby', {
+  headers: {
+    Authorization:
+      'Bearer BJ6aDrgieREkgR1s4AxSmCO6EDrSYSAOtfunJ4dhnYBIV9KukRxbOnWzzFP0haS1u6NhaxQuJrwnc0KgfjCtdPCQM30dOW0GOcWsL8-vqflR36McZ6SvmlLPCy3FXnYx',
+    'Access-Control-Allow-Origin': '*',
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
