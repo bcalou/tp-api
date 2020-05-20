37c8dd53fea93c35cd9c0d706c957d6d8376d743
anthony-roux <roux.anthony@hetic.net>
2020-05-21 00:24:55 +0200

    :alien: add fetch API

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
-console.log('Hello world');
+//console.log("Hello world");
+
+const API_URL = "https://api.thecatapi.com/v1/images/search";
+const $img = document.getElementById('img_cat');
+const $generatorBtn = document.querySelector('button');
+const $imgIsNotLoad = document.getElementById('error');
+
+console.log($imgIsNotLoad);
+$generatorBtn.addEventListener("click", (e) => {
+  getFactUrl()
+})
+
+
+function getFactUrl() {
+  return fetch(API_URL)
+    .then(res => {
+      if (res.ok) {
+        console.log(res);
+
+        res.json().then(data => {
+          $img.src = data[0].url
+        })
+      } else {
+        console.log(error);
+        $imgIsNotLoad.innerHTML = "le chat joue à cat-cache.."
+      }
+    });
+
+}
