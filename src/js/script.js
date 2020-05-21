ca40c4974d429dd70208b5d2ea22d5e3bb82d361
anthony-roux <roux.anthony@hetic.net>
2020-05-21 15:42:22 +0200

    :sparkles: add API_KEY

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,33 @@
 //console.log("Hello world");
 
-const API_URL = "https://api.thecatapi.com/v1/images/search";
 const $img = document.getElementById('img_cat');
 const $generatorBtn = document.querySelector('button');
 const $imgIsNotLoad = document.getElementById('error');
 
-console.log($imgIsNotLoad);
+
+const API_URL = "https://api.thecatapi.com/v1/images/search?0d04fbac-a247-4355-8c55-ce3950eacc48";
+
+const API_KEY = "0d04fbac-a247-4355-8c55-ce3950eacc48";
+
+const loadFetch = fetch(API_URL)
+  .then(res => {
+    if (res.ok) {
+      console.log(res);
+
+      res.json().then(data => {
+        $img.src = data[0].url
+      })
+    } else {
+      console.log(error);
+      $imgIsNotLoad.innerHTML = "le chat joue à cat-cache.."
+    }
+  });
+
 $generatorBtn.addEventListener("click", (e) => {
   getFactUrl()
 })
 
 
 function getFactUrl() {
-  return fetch(API_URL)
-    .then(res => {
-      if (res.ok) {
-        console.log(res);
-
-        res.json().then(data => {
-          $img.src = data[0].url
-        })
-      } else {
-        console.log(error);
-        $imgIsNotLoad.innerHTML = "le chat joue à cat-cache.."
-      }
-    });
-
+  return loadFetch;
 }
