3c6e530e982c46f067c6a71a14d2dfe8e12b4483
anthony-roux <roux.anthony@hetic.net>
2020-05-21 19:01:08 +0200

    :sparkles: add style class JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,31 +3,35 @@
 const $img = document.getElementById('img_cat');
 const $generatorBtn = document.querySelector('button');
 const $imgIsNotLoad = document.getElementById('error');
+const $factsContainer = document.getElementById('factsContainer');
+const $p = document.querySelector('p');
 
 
-const API_URL = "https://api.thecatapi.com/v1/images/search?0d04fbac-a247-4355-8c55-ce3950eacc48";
+const API_URL = "https://api.thecatapi.com/v1/images/search";
 
 const API_KEY = "0d04fbac-a247-4355-8c55-ce3950eacc48";
 
-const loadFetch = fetch(API_URL)
-  .then(res => {
-    if (res.ok) {
-      console.log(res);
+const mime_types = "?mime_types=gif?";
 
-      res.json().then(data => {
-        $img.src = data[0].url
-      })
-    } else {
-      console.log(error);
-      $imgIsNotLoad.innerHTML = "le chat joue à cat-cache.."
-    }
-  });
 
-$generatorBtn.addEventListener("click", (e) => {
-  getFactUrl()
+
+$generatorBtn.addEventListener("click", () => {
+  console.log("good");
+  getNewImage()
+  $factsContainer.classList.add('scale__container');
 })
 
 
-function getFactUrl() {
-  return loadFetch;
+function getNewImage() {
+  const loadFetch = fetch(`${API_URL}${mime_types}${API_KEY}`)
+    .then(res => {
+      if (res.ok) {
+        res.json().then(data => {
+          $img.src = data[0].url
+        })
+      } else {
+        console.log(error);
+        $imgIsNotLoad.innerHTML = "le chat joue à cat-cache.."
+      }
+    });
 }
