c2169ebb5b1e8d2b63ad81b77675157676fdbbff
Sebastienbezombes <bezombessebastien@gmail.com>
2020-05-21 20:54:58 +0200

    init axios

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+require('axios');
+let body = document.querySelector('body')
+/*
+axios.defaults.baseURL = 'https://api.github.com/'
+axios.get('users')
+  .then(res => console.log(res.data))
+  .catch(err => console.error(err))
+*/
+
+//axios.defaults.baseURL = 'https://api.github.com/'
+axios.get('http://api.open-notify.org/iss-now.json')
+  .then(res => console.log(res.data))
+  .catch(err => console.error(err))
