40c9474052e2b45b7e04580c154861cbca63ee6c
Sebastienbezombes <bezombessebastien@gmail.com>
2020-05-22 18:41:55 +0200

    add animes names in HTML and optimise res with function

--- src/js/script.js
+++ src/js/script.js
@@ -9,5 +9,14 @@ axios.get('users')
 
 axios.defaults.baseURL = 'https://kitsu.io/api/edge'
 axios.get('anime')
-  .then(res => console.log(res.data.data[5]))
+  .then(res => animeName(res))
   .catch(err => console.error(err))
+
+  const animeName = (res) => {
+    let resName = res.data.data;
+    for (i = 0; i < resName.length; i++) {
+      let h2 = document.createElement('h2')
+      h2.append(res.data.data[i].attributes.abbreviatedTitles)
+      body.append(h2)
+    }
+}
