d902ea4f30de701b09b27556d9dd7ae65b64577d
quentintrouve <trouve.quentin@gmail.com>
2020-05-23 01:17:39 +0200

    add project structure and create index.html style

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,16 +1,38 @@
-//const accessToken: string = "10218347135043962";
+// AXIOS
+
 import axios from "axios";
 
-function getHero(): void {
+// CONFIG API
+
+const URL_CORS: string = "https://cors-anywhere.herokuapp.com/";
+const URL_API: string = "https://superheroapi.com/api/";
+const ACCESS_TOKEN: string = "10218347135043962";
+
+// ÉLÉMENTS
+
+const $searchInput = document.getElementById("searchInput");
+const $searchButton = document.getElementById("searchButton");
+const $addButton = document.getElementById("addButton");
+
+// ACTIONS
+
+function getHeroName(): string {
+  return document.querySelector('input[name="nameHero"]').value;
+}
+
+function searchHeroByName(): void {
   axios
-    .get("https://superheroapi.com/api/10218347135043962/80")
+    .get(`${URL_CORS}${URL_API}${ACCESS_TOKEN}/search/${getHeroName()}`)
     .then(function (response: any): any {
-      let res = response.json();
-      console.log(res);
+      console.log(response);
     })
     .catch(function (error: any): any {
       console.log(error);
     });
 }
 
-getHero();
+// ÉVÈNNEMENTS
+
+$searchButton.addEventListener("click", () => {
+  searchHeroByName();
+});
