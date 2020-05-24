5a8204290561456248dcfe1315e8a8c52e00d10e
Sebastienbezombes <bezombessebastien@gmail.com>
2020-05-24 17:44:32 +0200

    init layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,27 @@
-require('axios');
-let body = document.querySelector('body')
-/*
-axios.defaults.baseURL = 'https://api.github.com/'
-axios.get('users')
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err))
-*/
+const $body = document.querySelector('body')
+const $content = document.querySelector('.content')
 
 axios.defaults.baseURL = 'https://kitsu.io/api/edge'
 axios.get('anime')
   .then(res => {
-    console.log(res.data.data)
     allFunctionsCalls(res)
-    console.log("www")
+    console.log(res.data.data)
   })
   .catch(err => console.error(err))
 
 const allFunctionsCalls = (res) => {
-  animeName(res)
-  animeImg(res)
+animeElement(res);
 }
 
-const animeName = (res) => {
-  let resName = res.data.data;
-  for (i = 0; i < resName.length; i++) {
-    let h2 = document.createElement('h2')
-    h2.append(resName[i].attributes.abbreviatedTitles)
-    body.append(h2)
-  }
-}
-
-const animeImg = (res) => {
-  let resImg = res.data.data
-  for (j = 0; j < resImg.length; j++) {
+const animeElement = (res) => {
+  let results = res.data.data;
+  results.forEach((result => {
     let image = document.createElement('img')
-    image.src = resImg[j].attributes.posterImage.medium
-    body.append(image) 
-  }
+    image.src = result.attributes.posterImage.medium
+    let div = document.createElement('div')
+    let h2 = document.createElement('h2')
+    h2.append(result.attributes.abbreviatedTitles)
+    div.append(h2)
+    $content.append(image, div)
+  }))
 }
-
-
-
- /* attributes:
-abbreviatedTitles: ["COWBOY BEBOP"]
-ageRating: "R"
-ageRatingGuide: "17+ (violence & profanity)"
-averageRating: "82.62"
-canonicalTitle: "Cowboy Bebop"
-coverImage:
-large: "https://media.kitsu.io/anime/cover_images/1/large.jpg?1519178801"
-
-*/
