b48afda7eb37d6da61587d66cef07d0efaa94b7a
Hakim <cheboub.h@gmail.com>
2020-05-24 16:50:35 +0200

    suite

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+
+const API_URL = "https://api.agify.io/";
+
+const $discoverAge = document.getElementById("buttonName");
+const $enterName = document.getElementById("enterName");
+const $responseAge = document.getElementById("responseAge");
+const $responseCount = document.getElementById("responseCount");
+
+
+
+$discoverAge.addEventListener("click", function (e) {
+    e.preventDefault();
+    let val = $enterName.value.toLowerCase();
+    fetch(`${API_URL}?name=${val}`)
+        .then(res => res.json())
+        .then(res => {
+            let response = res.age == null ? "ton prénom n'est pas connu ! " : " Voilà l'âge moyen de " + val + " dans le monde " + ": " + res.age + " ans. ";
+            $responseAge.value = response;
+            $responseAge.innerHTML = $responseAge.value;
+        }).catch(error => console.log(error));
+    $enterName.value = "";
+
+});
+
+
+
+
+
