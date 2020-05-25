27361c9ec6ed1e89dde7dd40277c4bbc9e4f8ccc
Shildya <nicolas@jesenberger.fr>
2020-05-25 11:59:06 +0200

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+const baseURL = 'https://api.rawg.io/api/';
+const main = document.querySelector('main');
+
+const url = ''.concat(baseURL);
+fetch(url)
+.then(result => result.json())
+.then(data => {
+    // code
+});
