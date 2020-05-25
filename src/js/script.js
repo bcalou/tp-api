d63441957e4b70daaf8df074f459ea2e39f52cde
BENSIMHON <yoan.benimhon@hetic.net>
2020-05-25 15:11:06 +0200

    bootstrap is now on and a liitle part of js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello world');
+$(document).ready(() =>{
+  $('#searchForm').on('submit' , (e)=>{
+    let searchText = $('#searchText').val();
+    getMeals(searchText);
+    e.preventDefault();
+  });
+  });
+  
+  
+  function getMeals(searchText){
+  axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+searchText)
+    .then((response) => {
+      console.log(response);
+      let meals = response.data;
+      console.log(meals);
+      
+      let output = '';
+      $.each(meals, (index, meal) => {
+        output += `
+        <div class="col-md-3">
+        <div class="well text-center">
+        <img src="${meals.strMealThumb}"/>
+        <h5>${meals.strMeal}</h5>
+        <a onclick="mealSelected('${meals.idMeal}')"class="btn btn-prymary" href="#">Meal Details</a>
+        </div>
+        </div>
+        `;
+      });
+        $('#meals').html(output);
+    })
+  };  
+  
