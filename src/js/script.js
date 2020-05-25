fffc8baedfed48b56ccd3281b263daccf9d0d386
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-25 18:20:36 +0200

    correction

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
-console.log('Hello world');
-
 const API_GEN3 = `https://pokeapi.co/api/v2/generation/3/`;
+const pokemonMax = 10;
 
 const fetchPokemon = () => {
-  for (let i = 1; i <= 15; i++) {
+  for (let i = 1; i <= pokemonMax; i++) {
     const API_URL = `https://pokeapi.co/api/v2/pokemon/${i}`;
 
     fetch(API_URL)
@@ -17,8 +16,6 @@ const fetchPokemon = () => {
           data.types.map(type => type.type.name),
           i
         );
-        // getColors(data.types.map(type => type.type.name));
-        // console.log(data);
       });
   }
 };
@@ -29,21 +26,10 @@ function getIdPokemon(name, id, type, i) {
   <h2 class="container__name">${name}</h2>
   <img src="https://pokeres.bastionbot.org/images/pokemon/${i}.png">
   
-  <p class="container__id">${id}</p>
-  <p class="container__id">${type}</p>
+  <p class="container__id">#${id}</p>
+  <p class="container__type">${type}</p>
   
 </div>`;
 }
 
-// function getColors(type) {
-//   const colors = {
-//     fire: '#f56',
-//     poison: '#ff6'
-//   };
-//   const container = document.querySelector('.container__id');
-//   const color = colors[`${type}`];
-//   container.style.color = color;
-// }
-
 fetchPokemon();
-// getColors();
