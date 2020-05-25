fd1dccd824938661f686702da3c34a472f865624
RedaHamouche <reda.hamouche@outlook.com>
2020-05-25 17:08:26 +0200

    stats

--- src/js/poke.js
+++ src/js/poke.js
@@ -1,7 +1,9 @@
 const $FORMNAME = document.querySelectorAll(".formName input");
 const $FINDNAME = document.querySelector(".findName");
+
 const $POKENAME = document.querySelector(".pokeTitle");
 const $POKEIMG = document.getElementById("pokeimg");
+const $POKEDESCRIP = document.getElementById("description");
 
 $FINDNAME.addEventListener("click", () => {
   const apiData = {
@@ -9,11 +11,12 @@ $FINDNAME.addEventListener("click", () => {
     type: "pokemon"
   };
 
-  const { url, type } = apiData;
+  const { url, type, form } = apiData;
 
-  const apiUrl = `${url}${type}/${$FORMNAME[0].value}`;
+  const apiPoke = `${url}${type}/${$FORMNAME[0].value}`;
 
-  fetch(apiUrl)
+  // CALL THE POKEMON LIST API ( GET METHOD BY DEFAULT)
+  fetch(apiPoke)
     .then(data => {
       if (data.ok) {
         return data.json();
@@ -23,10 +26,26 @@ $FINDNAME.addEventListener("click", () => {
     .then(pokemon => generateHtml(pokemon))
     .catch(error => catcherror());
 
+  // GENERATE HTML FROM THE API
+
   const generateHtml = data => {
     $POKENAME.innerHTML = data.name;
     $POKEIMG.src = data.sprites.front_default;
+
+    let html = `
+          <div class="pokeStats">
+          <div class="pokeId"> Id : ${data.id} </div>
+          <div class="pokeName"> Name : ${data.name} </div>
+          <div class="pokeHeight"> Height : ${data.height} </div>
+          <div class="pokeWeight"> Weight : ${data.weight} </div>
+          </div>
+          <button id="seeStats" class="seeStats">Voir la description</button>
+        </form>
+    `;
+    $POKEDESCRIP.innerHTML = html;
   };
+
+  // CATCH THE CASE WHERE THE POKEMON ISNT FIND IN THE API
   const catcherror = () => {
     $POKENAME.innerHTML = "Ce pokémon n'existe pas";
     $POKEIMG.src =
@@ -34,6 +53,12 @@ $FINDNAME.addEventListener("click", () => {
     $POKEIMG.style.width = "150px";
   };
   $FORMNAME[0].value = "";
+
+  const $FINSDESCRIP = document.getElementById("seeStats");
+
+  $FINSDESCRIP.addEventListener("click", () => {
+    console.log("it inzkn");
+  });
 });
 
 window.addEventListener("click", () => {
