ccb3feab0614d8f85649c631f4963dda22de1f98
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-25 15:44:35 +0200

    fetch pokemon with api

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
 console.log('Hello world');
+
+const API_GEN3 = `https://pokeapi.co/api/v2/generation/3/`;
+
+const fetchPokemon = () => {
+  for (let i = 1; i <= 15; i++) {
+    const API_URL = `https://pokeapi.co/api/v2/pokemon/${i}`;
+
+    fetch(API_URL)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        getIdPokemon(data.name, data.id, data.types.slice, i);
+      });
+  }
+};
+
+function getIdPokemon(name, id, type, i) {
+  document.querySelector('.content').innerHTML += ` 
+   <div class="container">
+  <h2 class="container__name">${name}</h2>
+  <img src="https://pokeres.bastionbot.org/images/pokemon/${i}.png">
+  
+  <p class="container__id">${id}</p>
+  <p class="container__id">${type}</p>
+</div>`;
+}
+
+fetchPokemon();
