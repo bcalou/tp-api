ff3c7aeebf9dfc1cfefa3e2993870ddf88cb9792
louisfleury <louis.fleury@hetic.net>
2020-05-25 11:45:43 +0200

    louis fleury

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+const API_URL = "https://dog.ceo/";
+const AMOUNT_OF_PICTURES = 3;
+
+const $RandomPictureButton = document.getElementById("GetARandomPicture");
+const $PicturesContainer = document.getElementById("PicturesContainer");
+
+function getPicturesUrl() {
+  return `${API_URL}/api/breeds/image/random/${AMOUNT_OF_PICTURES}`;
+}
+function getPictures() {
+  
+  return fetch(getPicturesUrl())
+    .then((res) => res.json())
+    .then((pictures) => showPictures(pictures));
+}
+
+function showPictures(pictures) {
+	$PicturesContainer.textContent = "Chargement en cours";
+	$PicturesContainer.innerHTML = "";
+  getPictures
+
+}
+
+$RandomPictureButton.addEventListener("click", showPictures(pictures) {
+  //toggleButton($RandomPictureButton);
+  //getPictures().then(() => toggleButton($RandomPictureButton));
+});
+
