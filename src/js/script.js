be53632c4afcf910126150697cb3f89b7beb4da1
louisfleury <louis.fleury@hetic.net>
2020-05-25 15:40:50 +0200

    changed my choice of API and tried to make it work

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,81 @@
-const API_URL = "https://dog.ceo/";
-const AMOUNT_OF_PICTURES = 3;
+//function httpGet(theURL) {
+//  var xmlHttp = new XMLHttpRequest();
+//  xmlHttp.open("GET", theURL, false); // false for synchronous request
+//  xmlHttp.send(null);
+//  return xmlHttp.responseText;
+//}
+//
+//function getRandomImage() {
+//  // get the json from the server
+//  var json = httpGet("https://dog.ceo/api/breeds/image/random");
+//
+//  // decode the json into an array
+//  var array = JSON.parse(json);
+//
+//  // get the image url from the array
+//  var url = array.message;
+//
+//  // get the image object
+//  var image = document.getElementById("dogImage");
+//
+//  // set the src of the image object
+//  image.src = url;
+//}
 
-const $RandomPictureButton = document.getElementById("GetARandomPicture");
-const $PicturesContainer = document.getElementById("PicturesContainer");
+const $RandomJokeButton = document.querySelector("button");
+const $JokeArea = document.getElementById("jokeArea");
+const API_URL = "https://api.chucknorris.io/jokes/random";
 
-function getPicturesUrl() {
-  return `${API_URL}/api/breeds/image/random/${AMOUNT_OF_PICTURES}`;
-}
-function getPictures() {
-  
-  return fetch(getPicturesUrl())
+function getJoke() {
+  $JokeArea.textContent = "Chargement en cours";
+
+  return fetch(getJokeUrl())
     .then((res) => res.json())
-    .then((pictures) => showPictures(pictures));
+    .then((jokes) => showJoke(jokes));
 }
 
-function showPictures(pictures) {
-	$PicturesContainer.textContent = "Chargement en cours";
-	$PicturesContainer.innerHTML = "";
-  getPictures
-
+function getJokeUrl() {
+  return `${API_URL}`;
 }
 
-$RandomPictureButton.addEventListener("click", showPictures(pictures) {
-  //toggleButton($RandomPictureButton);
-  //getPictures().then(() => toggleButton($RandomPictureButton));
+function showJoke(jokes) {
+  $JokeArea.textContent = "";
+
+  const $JokeFragment = document.createDocumentFragment();
+
+  for (let i = 0; i < jokes.length; i++) {
+    let joke = jokes[i];
+    (joke) => $JokeFragment.appendChild(getFactElement(jokes));
+  }
+
+  $JokeArea.appendChild($JokeFragment);
+}
+
+function createElement(options) {
+  const $element = document.createElement(options.type);
+
+  if (options.text) {
+    $element.textContent = options.text;
+  }
+
+  if (options.parent) {
+    options.parent.appendChild($element);
+  }
+
+  return $element;
+}
+
+function toggleButton($button) {
+  $button.disabled = !$button.disabled;
+}
+$RandomJokeButton.addEventListener("click", () => {
+  toggleButton($RandomJokeButton);
+  getJoke().then(() => toggleButton($RandomJokeButton));
 });
 
+function getJokeElement(joke) {
+  const $joke = createElement({ type: "p" });
+  createElement({ type: "article", text: jokes.value, parent: $joke });
+
+  return $joke;
+}
