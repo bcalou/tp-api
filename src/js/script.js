b56c36e8c8650d4a9dfb65927be0a2d6eebc8a3f
cpetillot <constance.petillot@hetic.net>
2020-05-25 10:14:10 +0200

    fetch API

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+const API_URL = "https://api.jokes.one/jod";
+const $jokeButton = document.getElementById("jokeButton");
+const $joke = document.getElementById("joke");
+
+// Fetch random joke from API
+function getJoke() {
+  $joke.textContent = "Loading";
+  return fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((joke) => showJoke(joke));
+}
+
+// Display joke into <p> tag
+function showJoke(joke) {
+  $jokeContainer.textContent = "";
+
+  const $JokeFragment = document.createDocumentFragment();
+
+  facts.forEach((joke) => $jokeFragment.appendChild(getJokeElement(joke)));
+
+  $jokeContainer.appendChild($jokeFragment);
+}
