2aa83f29e177d191a7322ed1fdf8fcf84bb0712a
Raphael <raphael.ferreira@hetic.net>
2020-05-25 14:30:57 +0200

    fetch api with input value

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,38 @@
-console.log('Hello world');
+const API_URL = "http://www.omdbapi.com/";
+const API_KEY = "12976118";
+
+const $movieTitleInput = document.getElementById("movie");
+const $searchButton = document.getElementById("search-button");
+
+// Return the input value
+function getMovieTitle(input) {
+  return input.value;
+}
+
+// function getLastPary(id) {
+//   axios
+//     .get(`${API_URL}/party/${id}`)
+//     .then((res) => {
+//       createArticle(res.data);
+//     })
+//     .then((error) => {
+//       console.log(error);
+//     });
+// }
+
+//http://www.omdbapi.com/?apikey=[yourkey]&
+
+function getMovieInfos(movie) {
+  axios
+    .get(`${API_URL}/?apikey=${API_KEY}&t=${movie}`)
+    .then((res) => {
+      console.log(res);
+    })
+    .then((error) => {
+      console.log(error);
+    });
+}
+
+$searchButton.addEventListener("click", () => {
+  getMovieInfos(getMovieTitle($movieTitleInput));
+});
