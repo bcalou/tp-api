cdf3649c4b90593ed9f1ceea35f46d4c5b48c38e
Shildya <nicolas@jesenberger.fr>
2020-05-26 10:43:57 +0200

    changed file structure, added API test

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 const baseURL = 'https://api.rawg.io/api/';
 const main = document.querySelector('main');
 
-const url = ''.concat(baseURL);
+const url = ''.concat(baseURL, 'games?ordering=-rating&page_size=10');
 fetch(url)
 .then(result => result.json())
-.then(data => {
-    // code
+.then((data) => {
+    data.results.forEach(game => {
+        main.textContent+= `[${game.name}-(${game.rating})]-`;
+    });
 });
