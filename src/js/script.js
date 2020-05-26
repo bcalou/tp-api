f83e642026c77a134d5135109201d14c9584a815
Ramtin Attar <ramtinattar13@gmail.com>
2020-05-26 14:17:09 +0200

    avancement

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-console.log("Goodbye world");
+const $callAPI = document.getElementById("call");
+const $showResult = document.getElementById("result");
+
+$callAPI.addEventListener("click", () => {
+  $showResult.innerHTML = "Loading...";
+  return fetch("https://the-one-api.herokuapp.com/v1/book")
+    .then((res) => res.json())
+    .then((books) => {
+      console.log(books);
+      $showResult.innerHTML = "";
+      var $bookList = books.docs;
+      $bookList.forEach(listBooks);
+    });
+});
+function listBooks(item, index) {
+  $showResult.innerHTML += (index += 1) + "- " + item.name + "<br>";
+}
