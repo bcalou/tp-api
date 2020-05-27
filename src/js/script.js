e384723f8bfa9d262a67ab513891f55595bb3e92
gravity-zero <gravity.neo@gmail.com>
2020-05-28 00:35:14 +0200

    fix placeholder image, replace if condition by ternaire, need to be refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,58 @@
 /**Doc API:
  * Everything : ?q=word + &language=
  */
+const $newsButton = document.getElementById("newsButton").addEventListener("click", (e) => {
+	e.preventDefault();
+	console.log('lol', $newsButton)
+	var searchs = document.getElementsByClassName("search")[0].value;
+  const placeholderimage = require("../asset/media/no-image-available.jpg");
+  const apiEveryThing = "http://newsapi.org/v2/everything";
+  const sort = "popularity";
+  const lang = "fr";
+  var requestSearch = encodeURI(searchs);
+  const apiKey = "apiKey=d182478c672e466298066e128a76e0bd";
+	const $articleContainer = document.getElementsByClassName("articleContainer")[0];
 
-	const valid = document.getElementById("val").addEventListener("click", function () {
-			var searchs = document.getElementsByClassName("search")[0].value;
-			const apiEveryThing = "http://newsapi.org/v2/everything";
-			const sort = "popularity";
-			const lang = "fr";
-			var requestSearch = encodeURI(searchs);
-			const apiKey = "apiKey=d182478c672e466298066e128a76e0bd";
-			const $articleContainer = document.getElementsByClassName("articleContainer")[0];
+  fetch(`${apiEveryThing}?q=${requestSearch}&language=${lang}&sortBy=${sort}&pageSize=10&page=1&${apiKey}`)
+    .then((data) => data.json())
+    .then((response) => {
+      console.log(response);
+      let status = response.status;
+      var articles = status ? response.articles : noContent();
 
-			fetch(
-				`${apiEveryThing}?q=${requestSearch}&language=${lang}&sortBy=${sort}&pageSize=10&page=1&${apiKey}`
-			)
-				.then((data) => data.json())
-				.then((response) => {
-					console.log(response);
-					var status = response.status;
-					if (status !== "error") {
-						var articles = response.articles;
+        //Save value for each article
+        articles.forEach((response) => {
+          var images = (response.urlToImage ? image = response.urlToImage : image = placeholderimage);
+          var sources = response.source.name;
+          var titles = response.title;
+          var descriptions = response.description;
+          var urls = response.url;
 
-						//Save value for each article
-						articles.forEach((response) => {
-							if (response.urlToImage !== "null")
-							var image = response.urlToImage;
-							else
-							var image = "../asset/media/no-image-available.jpg";
-							var source = response.source.name;
-							var title = response.title;
-							var description = response.description;
-							var url = response.url;
+        //Create container and html element for each article
+          var article = $articleContainer.appendChild(document.createElement("article"));
+          var link = article.appendChild(document.createElement("a"));
+          var hOne = article.appendChild(document.createElement("h1"));
+          var img = article.appendChild(document.createElement("img"));
+          var description = article.appendChild(document.createElement("ul"));
+          var articleContent = description.appendChild(document.createElement("li"));
+          var newspaper = description.appendChild(document.createElement("li"));
 
-						//Create container and html element for each article
-							var div = $articleContainer.appendChild(document.createElement("div"));
-							var link = div.appendChild(document.createElement("a"));
-							var hOne = div.appendChild(document.createElement("h1"));
-							var img = div.appendChild(document.createElement("img"));
-							var article = div.appendChild(document.createElement("ul"));
-							var articleContent = article.appendChild(document.createElement("li"));
-							var newspaper = article.appendChild(document.createElement("li"));
+          //Check if we have a title
+          if (titles !== "undefined") {
+            link.setAttribute = ('href', `"${urls}"`);
+            img.setAttribute = ('src', `"${images}"`);
+            link.href = `${urls}`;
+            hOne.textContent += `${titles}`;
+            img.src = `${image}`;
+            articleContent.textContent += `${descriptions}`;
+            newspaper.textContent += `${sources}`;
+            link.appendChild(hOne);
+            link.appendChild(img);
+            }
+        });
+    });
+});
 
-							//Check if we have a title
-							if (title !== "undefined") {
-								link.setAttribute = ('href', `"${url}"`);
-								img.setAttribute = ('src', `"${image}"`);
-								link.href = `${url}`;
-								hOne.textContent += `${title}`;
-								img.src = `${image}`;
-								articleContent.textContent += `${description}`;
-								newspaper.textContent += `${source}`;
-								link.appendChild(hOne);
-								link.appendChild(img);
-								}
-						});
-					}
-				});
-		});
-
+function noContent(){
+  hOne.textContent += `"Articles correspondants : 0"`;
+}
