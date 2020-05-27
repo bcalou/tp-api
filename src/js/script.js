7731047a9fa7951fb4d0e402e57d23ff0fd8848c
eunji <eunji@eunjiui-macbookpro.home>
2020-05-27 17:43:17 +0200

    Add: celsius and input for city

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 let todaycontent = document.getElementById("todaycontent");
 let latercontent = document.getElementById("latercontent");
+let citycontent = document.getElementById("citycontent");
 
 function get(theUrl) {
   var xmlHttp = new XMLHttpRequest();
@@ -8,43 +9,44 @@ function get(theUrl) {
   return xmlHttp.responseText;
 }
 
+var today = document.getElementById("today");
+today.addEventListener("click", todayclick, false);
+
 function todayclick() {
   let url =
     "http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=247b84c35424911c499563ba4305dfe2&units=metric";
   let text = get(url);
   let json = JSON.parse(text);
-  var textnode = document.createTextNode(json.main.temp);
+  var textnode = document.createTextNode(json.main.temp + "°");
+  todaycontent.innerHTML = "";
   todaycontent.appendChild(textnode);
 }
 
-var today = document.getElementById("today");
-today.addEventListener("click", todayclick, false);
+var later = document.getElementById("in5days");
+later.addEventListener("click", laterclick, false);
 
 function laterclick() {
   let url =
     "http://api.openweathermap.org/data/2.5/forecast?id=2988507&appid=247b84c35424911c499563ba4305dfe2&units=metric";
   let text = get(url);
   let json = JSON.parse(text);
-  var textnode = document.createTextNode(
-    JSON.stringify(json.list[1].main.temp)
-  );
+  var textnode = document.createTextNode(json.list[1].main.temp + "°");
+  latercontent.innerHTML = "";
   latercontent.appendChild(textnode);
 }
 
-var later = document.getElementById("in5days");
-later.addEventListener("click", laterclick, false);
-
-let input = document.getElementById("city").value;
-input = Object.keys(input);
-console.log(input);
+var yourCity = document.getElementById("yourCity");
+yourCity.addEventListener("click", cityclick, false);
 
 function cityclick() {
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=247b84c35424911c499563ba4305dfe2&units=metric`;
+  var inputVal = document.getElementById("city").value;
+  let url =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    inputVal +
+    "&appid=247b84c35424911c499563ba4305dfe2&units=metric";
   let text = get(url);
   let json = JSON.parse(text);
-  var textnode = document.createTextNode(json.main.temp);
+  var textnode = document.createTextNode(json.main.temp + "°");
+  citycontent.innerHTML = "";
   citycontent.appendChild(textnode);
-  console.log(input);
 }
-
-yourCity.addEventListener("click", cityclick, false);
