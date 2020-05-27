54cb423f0a00617943e40bd014d4c9a313a660b5
Sebastienbezombes <bezombessebastien@gmail.com>
2020-05-27 19:02:39 +0200

    optimise TypeScript file btns with condition ternaire

--- src/js/btns.ts
+++ src/js/btns.ts
@@ -1,8 +1,6 @@
 const $btns = document.querySelectorAll('.pages');
 const $h1 = document.querySelector('h1')
-const h1Txt1 = "Search"
-const h1Txt2 = "App"
-console.log($btns)
+const [h1Txt1, h1Txt2] = ["Search", "App"]
 
 $btns.forEach((btn, index) => {
     $btns[0].classList.add('active')
@@ -10,19 +8,6 @@ $btns.forEach((btn, index) => {
         let btnOn = document.querySelector('.active')
         btnOn ? btnOn.classList.remove('active') : ''
         btn.classList.add('active')
-
-        if (btnOn) {
-            switch (index) {
-                case 0 : 
-                    $h1.textContent = `${h1Txt1} Animes ${h1Txt2}`
-                    break
-                case 1 :
-                    $h1.textContent = `${h1Txt1} Mangas ${h1Txt2}`
-                    break
-                }
-            }
-        }
-    )})
-
-
-//let btnOn = document.querySelector('.active')
+        btnOn ? index === 0 ? $h1.textContent = `${h1Txt1} Animes ${h1Txt2}` : $h1.textContent = `${h1Txt1} Mangas ${h1Txt2}` : ''
+    })
+})
