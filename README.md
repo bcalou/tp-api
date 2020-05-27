821b51fb462bd36ecf2e0503d79e66d4afeebc1e
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-05-28 01:08:05 +0200

    now you can search you pokemon with the image of it and you can have access toother description of the pokemon

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ npm run dev
 
 ## But de l'application en une phrase
 
-À remplir
+Vous permet d'avoir accés a un certain nombres de pokémon
 
 ## URL de l'API utilisée
 
-URL: https://pokeapi.co/api/v2
+URL: https://pokeapi.co/api/v2/pokemon
 
 ## Liste des routes sollicitées
 
@@ -23,4 +23,7 @@ URL: https://pokeapi.co/api/v2
 
 ## Fonctionnement détaillé de l'application
 
-À remplir : que peux faire votre prototype ?
+Que peux faire votre prototype ?
+
+Mon prototype permet de faire une recherche de pokémon via la search bar
+mais aussi au click sur le bouton "see more" d'avoir accés a d'autre information en relation avec le pokemon voulu.
--- src/js/script.js
+++ src/js/script.js
@@ -1,86 +1,82 @@
-console.log('Hello world');
 //create const to stock URL from API
-const Api_Url = "https://pokeapi.co/api/v2/pokemon"
-const $container = document.querySelector('.container')
-const $infoContainer = document.querySelector('.container__info')
-const $infoContent = document.querySelector('.container__infos')
-const $infoTitle = document.querySelector('.container__infoTitle')
-const search = document.querySelector(".searchBar__click")
-const $input = document.querySelector('.searchBar__input[name=pokemon]')
+const Api_Url = "https://pokeapi.co/api/v2/pokemon";
+const $container = document.querySelector('.container');
+const search = document.querySelector(".searchBar__click");
+const $input = document.querySelector('.searchBar__input[name=pokemon]');
 
+let DATA = [];
 
-//this function do a request to get object from API
-getPokemons()
-function getPokemons(){
-  try {
-    return fetch(`${Api_Url}/?limit=id`)
-    .then(Response => Response.json())
-    .then(data => {
-      showPokemon(data.results)
-      showData(data.results)
+// retrieve data from API
+Promise.all(
+  Array.from(
+    { length: 50 },
+    (_, index) => fetch(`${Api_Url}/${index + 1}`)
+    .then(value => value.json())
+    )
+    )
+    .then(values => {
+      DATA = values;
+      showPokemon(DATA);
+      console.log(DATA);
     })
-  }
-    catch (error) {
-    console.error(error)
-  }
-}
-let DATA = []
-function showData(data) {
-  DATA.push(...data)
-  // console.log(DATA)
+    .catch((error) => {
+      console.log(error);
+    });
+
+let $buttons = [];
+let events = [];
+
+function removeButtonsListeners() {
+  // Avoid memory leak
+  $buttons.forEach((button, index) => {
+    button.removeEventListener("click", events[index]);
+  });
+  $buttons = [];
+  events= [];
 }
 
 //this function do a request to show all the pokemon dynamically in HTML
 function showPokemon(pokemon) {
-    $container.innerHTML = 
-    pokemon.map((element) => {
-      // console.log(element)
-      return`
-        <div class="container__cardsPokemon">
-          <p class="container__namePokemon">${element.name}</p>
-          <img class="container__imgPokemon" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.indexOf(element)+1}.png"/>
-          <button class="container__button" onclick="getThePokemon('${element.url}')">see more</button>
-        </div>
-      `
-    }).join('');
-  }
 
+  // append card to html
+  $container.innerHTML = pokemon.map(element => {
+    events.push(() => showInfo(element));
+    return `
+      <div class="container__cardsPokemon">
+        <p class="container__namePokemon">${element.name}</p>
+        <img class="container__imgPokemon" src="https://pokeres.bastionbot.org/images/pokemon/${element.id}.png"/>
+        <button class="container__button">see more</button>
+      </div>`;
+  }).join('');
 
-window.getThePokemon = function(URL){
-  try {
-    return fetch(URL)
-    .then(res => res.json())
-    .then(data => showInfo(data))
-  } catch (error) {
-    console.log(error)
-  }
+  // register event listeners
+  $buttons = Array.from(document.querySelectorAll(".container__button"));
+  $buttons.forEach((button, index) => {
+    button.addEventListener("click", events[index]);
+  });
 }
 
-function showInfo(infos){
+function showInfo(pokemon) {
+  removeButtonsListeners();
+
   $container.innerHTML = 
-    `
+  `
     <section class="container__infos">
     <a href=""><button>retour au menu</button></a>
     <div class="container__cardsPokemon">
-      <h2 class="container__namePokemon">${infos.name}</h2>
-      <img class="container__imgPokemon" src="https://pokeres.bastionbot.org/images/pokemon/${infos.id}.png"/>
+      <h2 class="container__namePokemon">${pokemon.name}</h2>
+      <img class="container__imgPokemon" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
       <div class="container__descriptions">
-        <p class="container__caract">${infos.weight} weight</p>
-        <p class="container__caract">${infos.height} height</p>
+        <p class="container__caract">${pokemon.weight} weight</p>
+        <p class="container__caract">${pokemon.height} height</p>
       </div>
     </div>
-    </section>
-    `
-  console.log(infos)
+    </section>`;
 }
 
-// const $addPokemon = document.querySelector('.searchBar__click')
-
-search.addEventListener("click", function(event) {
+search.addEventListener("click", event => {
   event.preventDefault();
-  str = $input.value;
-    const searchPokemon = DATA.filter(el => {
-      return el.name.includes(str)
-    })
-    showPokemon(searchPokemon)
+  const searchPokemon = DATA.filter(el => el.name.includes($input.value));
+  removeButtonsListeners();
+  showPokemon(searchPokemon);
 });
