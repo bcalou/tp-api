773cf507eaf54a1080facc71e98249917be567d7
BENSIMHON <yoan.benimhon@hetic.net>
2020-05-27 09:41:41 +0200

    add new html page and nex pb

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,37 @@
-$(document).ready(() =>{
-  $('#searchForm').on('submit' , (e)=>{
-    let searchText = $('#searchText').val();
+$(document).ready(() => {
+  $("#searchForm").on("submit", (e) => {
+    let searchText = $("#searchText").val();
     getMeals(searchText);
     e.preventDefault();
   });
-  });
-  
-  
-  function getMeals(searchText){
-  axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+searchText)
+});
+
+function getMeals(searchText) {
+  axios
+    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchText)
     .then((response) => {
       console.log(response);
       let meals = response.data.meals;
       console.log(meals);
-      
-      let output = '';
+
+      let output = "";
       $.each(meals, (index, meal) => {
         output += `
         <div class="col-md-3">
         <div class="well text-center">
         <img src="${meal.strMealThumb}"/>
         <h5>${meal.strMeal}</h5>
-        <a onclick="mealSelected('${meal.idMeal}')"class=" btn btn-primary" href="#">Meal Details</a>
+        <a onclick="mealSelected('${meal.idMeal}')" class=" btn btn-primary" href="#">Meal Details</a>
         </div>
         </div>
         `;
       });
-        $('#meals').html(output);
-    })
-  };  
-  
+      $("#meals").html(output);
+    });
+}
+
+function mealSelected(id) {
+  sessionStorage.setItem("mealId", id);
+  window.location = "meal.html";
+}
+
