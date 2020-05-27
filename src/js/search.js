a0339343e86a1366a7948466b1e705695d98f844
Nstampfli <nathan.stampfli@hetic.net>
2020-05-27 17:05:36 +0200

    filters is good we can us both same time

--- src/js/search.js
+++ src/js/search.js
@@ -5,25 +5,47 @@ const numberOfPokemon = 807;
 
 
 
-if ($searchBar !== null) {
-  $searchBar.addEventListener('input', () => {+
-    $pokemonCards.forEach(pokemonCard =>{
-      var pokemonName = pokemonCard.querySelector(".pokemon__name").textContent;
+$searchBar.addEventListener('input', () => {
+  $pokemonCards.forEach(pokemonCard =>{
+    var pokemonName = pokemonCard.querySelector(".pokemon__name").textContent;
+    if (selectTypes.value == 0) {
       if(pokemonName.startsWith($searchBar.value.toLowerCase())){
         pokemonCard.classList.remove("hidden");
       }
       else{
         pokemonCard.classList.add('hidden');
       };
-    })
-    if(document.querySelectorAll(".pokemon__card.hidden").length ===numberOfPokemon){
-      document.querySelector('.noResult').classList.add('visible');
     }
-    else {
-      document.querySelector('.noResult').classList.remove('visible');
-  };  
+    if (selectTypes.value !== 0) {
+      const pokemonTypes = pokemonCard.querySelectorAll(".type");
+      if(pokemonTypes.length === 1){
+        if(pokemonTypes[0].textContent == selectTypes.value && pokemonName.startsWith($searchBar.value.toLowerCase())){
+        pokemonCard.classList.remove("hidden");
+        }
+        else if (selectTypes.value ==0 && pokemonName.startsWith($searchBar.value.toLowerCase())) {
+          pokemonCard.classList.remove("hidden");
+        }
+        else{
+          pokemonCard.classList.add('hidden');
+        }
+      }
+      if(pokemonTypes.length === 2){
+        if(pokemonTypes[0].textContent == selectTypes.value  && pokemonName.startsWith($searchBar.value.toLowerCase()) || pokemonTypes[1].textContent == selectTypes.value  && pokemonName.startsWith($searchBar.value.toLowerCase())){
+          pokemonCard.classList.remove("hidden");
+        }
+          else if (selectTypes.value ==0 && pokemonName.startsWith($searchBar.value.toLowerCase())) {
+            pokemonCard.classList.remove("hidden");
+          }
+          else{
+            pokemonCard.classList.add('hidden');
+          }
+      }
+      
+    }
   })
-}
+  noResultDisplay();  
+})
+
 
 
 
@@ -31,19 +53,9 @@ if ($searchBar !== null) {
 selectTypes.addEventListener('change',()  =>{
   $pokemonCards.forEach(pokemonCard =>{
     const pokemonTypes = pokemonCard.querySelectorAll(".type");
-    if(pokemonTypes.length === 1){
-      if(pokemonTypes[0].textContent == selectTypes.value){
-      pokemonCard.classList.remove("hidden");
-      }
-      else if (selectTypes.value ==0 ) {
-        pokemonCard.classList.remove("hidden");
-      }
-      else{
-        pokemonCard.classList.add('hidden');
-      }
-    }
-    if(pokemonTypes.length === 2){
-      if(pokemonTypes[0].textContent == selectTypes.value || pokemonTypes[1].textContent == selectTypes.value){
+    if($searchBar.value == ''){
+      if(pokemonTypes.length === 1){
+        if(pokemonTypes[0].textContent == selectTypes.value){
         pokemonCard.classList.remove("hidden");
         }
         else if (selectTypes.value ==0 ) {
@@ -53,6 +65,55 @@ selectTypes.addEventListener('change',()  =>{
           pokemonCard.classList.add('hidden');
         }
       }
+      if(pokemonTypes.length === 2){
+        if(pokemonTypes[0].textContent == selectTypes.value || pokemonTypes[1].textContent == selectTypes.value){
+          pokemonCard.classList.remove("hidden");
+          }
+          else if (selectTypes.value ==0 ) {
+            pokemonCard.classList.remove("hidden");
+          }
+          else{
+            pokemonCard.classList.add('hidden');
+          }
+      }
+    }
+    if($searchBar.value !== ''){
+      var pokemonName = pokemonCard.querySelector(".pokemon__name").textContent;
+      if(pokemonTypes.length === 1){
+        if(pokemonTypes[0].textContent == selectTypes.value && pokemonName.startsWith($searchBar.value.toLowerCase())){
+        pokemonCard.classList.remove("hidden");
+        }
+        else if (selectTypes.value ==0 && pokemonName.startsWith($searchBar.value.toLowerCase())) {
+          pokemonCard.classList.remove("hidden");
+        }
+        else{
+          pokemonCard.classList.add('hidden');
+        }
+      }
+      if(pokemonTypes.length === 2){
+        if(pokemonTypes[0].textContent == selectTypes.value  && pokemonName.startsWith($searchBar.value.toLowerCase()) || pokemonTypes[1].textContent == selectTypes.value  && pokemonName.startsWith($searchBar.value.toLowerCase())){
+          pokemonCard.classList.remove("hidden");
+          }
+          else if (selectTypes.value ==0 && pokemonName.startsWith($searchBar.value.toLowerCase())) {
+            pokemonCard.classList.remove("hidden");
+          }
+          else{
+            pokemonCard.classList.add('hidden');
+          }
+      }
+    }
   }) 
+  noResultDisplay();
 })
 
+  
+
+function noResultDisplay() {
+  const $noResult = document.querySelector('.noResult');
+  if(document.querySelectorAll(".pokemon__card.hidden").length ===numberOfPokemon){
+    $noResult.classList.add('visible');
+  }
+  else {
+    $noResult.classList.remove('visible');
+  }; 
+}
