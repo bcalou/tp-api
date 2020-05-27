5038d0cf1b40251726a4f8ad7e8e4ae4c99285ae
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-05-27 19:35:37 +0200

    change name of functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-document.getElementById("getText").addEventListener("click", getText);
-document.getElementById("getUsers").addEventListener("click", getUsers);
-document.getElementById("getPosts").addEventListener("click", getPosts);
+document.getElementById("getText").addEventListener("click", getHouse);
+document.getElementById("getUsers").addEventListener("click", getSpells);
+document.getElementById("getPosts").addEventListener("click", getCharacters);
 // document.getElementById("addPost").addEventListener("submit", addPost);
 
 API_URL = "https://www.potterapi.com/v1/";
-function getText() {
+API_KEY = "/?key=$2a$10$ic.wdemGvyXtUA5jugCjreRaG1HZKSuT3wLAia24ElmSuCFyUo3Xq";
+
+function getHouse() {
 	fetch(API_URL + "sortingHat")
 		.then((response) => response.text())
 		.then((data) => {
@@ -14,37 +16,34 @@ function getText() {
 	console.log(123);
 }
 
-function getUsers() {
-	fetch(
-		"https://www.potterapi.com/v1/spells?key=$2a$10$ic.wdemGvyXtUA5jugCjreRaG1HZKSuT3wLAia24ElmSuCFyUo3Xq"
-	)
+function getSpells() {
+	fetch(API_URL + "spells" + API_KEY)
 		.then((response) => response.json())
 		.then((data) => {
 			let outpout = '<h2 class="mb-4">Tu trouveras ici la liste des sorts</h2>';
-			data.forEach(function (user) {
+			data.forEach(function (spells) {
 				outpout += `<ul class="list-group mb-3">
-				<li class="list-group-item">Nom : ${user.spell}</li>
-				<li class="list-group-item">Effet : ${user.effect}</li>
-				<li class="list-group-item">Type : ${user.type}</li>
+				<li class="list-group-item">Nom : ${spells.spell}</li>
+				<li class="list-group-item">Effet : ${spells.effect}</li>
+				<li class="list-group-item">Type : ${spells.type}</li>
 				</ul>`;
 			});
+
 			document.getElementById("outpout").innerHTML = outpout;
 		});
 }
 
-function getPosts() {
-	fetch(
-		"https://www.potterapi.com/v1/characters?key=$2a$10$ic.wdemGvyXtUA5jugCjreRaG1HZKSuT3wLAia24ElmSuCFyUo3Xq"
-	)
+function getCharacters() {
+	fetch(API_URL + "characters" + API_KEY)
 		.then((response) => response.json())
 		.then((data) => {
 			let outpout = '<h2 class="mb-4">La liste de tous les élèves</h2>';
 			console.log(data);
-			data.forEach(function (post) {
+			data.forEach(function (character) {
 				outpout += `
 				<div class="card card-body mb-3>
-				<h3>${post.name}</h3>
-				<p>${post.house}</p>
+				<h3>${character.name}</h3>
+				<p>${character.house}</p>
 				</div>`;
 			});
 			document.getElementById("outpout").innerHTML = outpout;
