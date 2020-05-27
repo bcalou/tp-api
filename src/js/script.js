e8d01d15739316a8d1f14ee73f50bb48f3fdca3e
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-05-28 00:56:41 +0200

    add elements, fetch ‘search, results, details of chosen recipe’

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const searchbutton = document.querySelector(".searchBox__searchButton");
   const searchBox = document.querySelector(".searchBox__searchTerm");
   const foodsContainer = document.querySelector(".popular__food");
+  const headerResult = document.querySelector(".displayResult__searchValue");
+  const results = document.querySelector(".resultSearch__details");
+  const resultFoodName = document.querySelector(".resultSearch__name");
+  const resultFoodPicture = document.querySelector(".resultSearch__picture");
+  const resultFoodCookTime = document.querySelector(".resultSearch__time");
+  const resultFoodIngredients = document.querySelector(".resultSearch__ingredients");
+  const resultFoodCookInstruction = document.querySelector(".resultSearch__cookInstruction");
+  const sectionSearch = document.querySelector(".search");
+ 
+
+
+  /************* get ************/
+  
+  const categoryMainCourse = document.querySelector(".categoryList__mainCourse");
+  const categoryBreakfast = document.querySelector(".categoryList__breakfast");
+  const categoryAppetizer = document.querySelector(".categoryList__appetizer");
+  const categoryFingerFood = document.querySelector(".categoryList__fingerFood");
+  const categoryDessert = document.querySelector(".categoryList__breakfast");
+  const categoryVegetarian = document.querySelector(".categoryList__vegetarian");
+  const categoryVegan = document.querySelector(".categoryList__vegan");
+  
+
+
+
 
   /************* fetch search ************/
 
@@ -13,12 +37,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   function searchRecipes() {
  
-    fetch(`${APIurl}/recipes/search?query=${searchBox.value}&number=6&apiKey=${API_key}`)
+    fetch(`${APIurl}/recipes/search?query=${searchBox.value}&number=${numberOfRecipes}&apiKey=${API_key}`)
       .then(response => response.json())
       .then(recipes => {
         console.log(recipes.results)
         let divRecipies = recipes.results.map((recipe) => {
-          return generateRecipeDiv(recipe)
+          return generateRecipeDiv(recipe, true)
         })
         console.log(divRecipies);
         divRecipies.forEach(recipeDiv => {
@@ -28,7 +52,73 @@ document.addEventListener("DOMContentLoaded", function (event) {
   
   }
 
-  function generateRecipeDiv(recipe) {
+  function getCookInstruction(recipe) {
+    fetch(`${APIurl}/recipes/${recipe.id}/analyzedInstructions?apiKey=${API_key}`)
+      .then(response => response.json())
+      .then(instructionInfo => {
+        console.log(instructionInfo);
+
+        //put foreach function that declaired ourside
+
+        instructionInfo.forEach(instruction => {
+          instruction.steps.forEach(stepInfo => {
+            generateInstructionText(stepInfo);
+          })
+        })
+      });
+}
+
+  function detailsRecipe(recipe) {
+    console.log(recipe);
+    let divDetails = generateRecipeDiv(recipe, false);
+    results.innerHTML = ""
+    results.appendChild(divDetails);
+    resultFoodIngredients.innerHTML = ""
+    getIngredients(recipe);
+    resultFoodCookInstruction.innerHTML = ""
+    getCookInstruction(recipe);
+  }
+
+
+
+  function getIngredients(recipe) {
+    fetch(`${APIurl}/recipes/${recipe.id}/ingredientWidget.json?apiKey=${API_key}`)
+      .then(response => response.json())
+      .then(ingredientsInfo=> {
+        //put function that declaired outside
+        ingredientsInfo.ingredients.forEach(ingredient => {
+          generateIngredientText(ingredient);
+        })
+    });
+  }
+        
+//declare the function for make ingredients
+        
+  function generateIngredientText(ingredient) {
+    let ingredientsInfo = document.createElement('p');
+    ingredientsInfo.innerHTML = `${ingredient.name} ${ingredient.amount.us.unit} ${ingredient.amount.us.value}`;
+    resultFoodIngredients.appendChild(ingredientsInfo);
+    
+      
+  };
+
+
+// declaire the function for make instruction
+  
+function generateInstructionText(stepInfo) {
+  let instructionsDetails = document.createElement('p');
+  instructionsDetails.innerHTML = `${stepInfo.number} ${stepInfo.step}`;
+  resultFoodCookInstruction.appendChild(instructionsDetails);
+  
+
+  };
+  
+
+  
+  
+/************* create div of recipe on page as result ************/
+  
+  function generateRecipeDiv(recipe, buttonDisplay) {
     let divRecipe = document.createElement('div')
     divRecipe.className ='foopopularFood__type foodType'
     
@@ -46,18 +136,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
     cookTime.innerText = recipe.readyInMinutes;
     divRecipe.appendChild(cookTime);
 
-    let buttonSeeRecipe = document.createElement('button');
+   
+
+    if (buttonDisplay === true) {
+      let buttonSeeRecipe = document.createElement('button');
+    buttonSeeRecipe.className = "buttonSeeRecipe";
     buttonSeeRecipe.innerText = 'See Recipe';
     divRecipe.appendChild(buttonSeeRecipe);
+    
+    buttonSeeRecipe.addEventListener('click', () => {
+      // change the class
+    
+          detailsRecipe(recipe)
+        })
 
-  
-
+    }
     return divRecipe
+
   }
 
   searchbutton.addEventListener('click', () => {
-
-
-    searchRecipes();
+    resultFoodIngredients.innerHTML = ""
+    searchRecipes()
   })
+
+
+  
 });
