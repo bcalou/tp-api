90e48e8e702d09428de548436f9e29117ed8e8a5
Teo Lugat <teo.lugat@hetic.net>
2020-05-27 18:39:10 +0200

    global news request + display news

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,8 @@ const covidUrl = "https://covid-19-data.p.rapidapi.com/";
 const smartableUrl = "https://api.smartable.ai/coronavirus/";
 const globalInfos = document.getElementById("globalInfos");
 const countryInfos = document.getElementById("countryInfos");
-const countryWrapper = document.querySelector(".country-wrapper");
-const sectionArrow = document.querySelector(
-  ".section--country .section__logoArrow"
-);
+const countryWrapper = document.querySelector(".country__infosWrapper");
+const sectionArrow = document.querySelector(".country__logoArrow");
 
 sectionArrow.addEventListener("click", () =>
   openCloseSection(countryWrapper, sectionArrow)
@@ -28,26 +26,32 @@ const totalReq = () => {
     },
   })
     .then((res) => res.json())
-    .then((res) => displayGlobalNews(res[0]))
+    .then((res) => displayGlobalStats(res[0]))
     .catch((err) => {
       console.log(err);
     });
 };
 
-const displayGlobalNews = (global) => {
+const displayGlobalStats = (global) => {
   globalInfos.innerHTML = `${createStatsTemplate(global)}`;
 };
 
-document.addEventListener("DOMContentLoaded", getLatestTotalNews);
+document.addEventListener("DOMContentLoaded", () => {
+  getLatestTotalNews();
+  getLatestArticles("global", displayGlobalArticles);
+});
 
 const getCountry = (form) => {
   const countryName = form.querySelector("input[name]").value.toLowerCase();
-  if (countryName) {
-    getLatestCountryNews(countryName);
+  if (countryName && countryName !== ("united-states" || "united states")) {
+    getLatestCountryStats(countryName);
+  }
+  if (countryName == ("united-states" || "united states")) {
+    getLatestCountryStats("usa");
   }
 };
 
-const getLatestCountryNews = (countryName) => {
+const getLatestCountryStats = (countryName) => {
   fetch(`${covidUrl}country?name=${countryName}`, {
     headers: {
       "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
@@ -56,29 +60,41 @@ const getLatestCountryNews = (countryName) => {
   })
     .then((res) => res.json())
     .then((res) => {
-      displayCountryNews(res[0]);
-      getLatestCountryArticles(res[0].code);
+      displayCountryStats(res[0]);
+      getLatestArticles(res[0].code, displayCountryArticles);
     })
     .catch((err) => console.log(err));
 };
 
-const displayCountryNews = (country) => {
+const displayCountryStats = (country) => {
   countryInfos.innerHTML = `${createStatsTemplate(country)}`;
 };
 
-const getLatestCountryArticles = (countryCode) => {
+const displayGlobalArticles = (articles) => {
+  const globalNews = document.querySelector(".global__articles");
+  for (let i = 0; i < 3; i++) {
+    const child = createArticleTemplate(articles.news[i]);
+    child.classList.add("articles__item--global");
+    globalNews.appendChild(child);
+  }
+};
+
+const getLatestArticles = (countryCode, display) => {
   fetch(`https://api.smartable.ai/coronavirus/news/${countryCode}`, {
     headers: {
       "Subscription-Key": "02e702b6f6a44031bf3037c3f3f0e25d",
     },
   })
     .then((res) => res.json())
-    .then((res) => displayCountryArticles(res))
+    .then((res) => display(res))
     .catch((err) => console.log(err));
 };
 
 const displayCountryArticles = (articles) => {
-  const articlesContainer = document.querySelector(".country-articles");
+  const articlesContainer = document.querySelector(".country__articles");
+  articlesContainer
+    .querySelectorAll("article")
+    .forEach((article) => articlesContainer.removeChild(article));
   articles.news.forEach((article) =>
     articlesContainer.appendChild(createArticleTemplate(article))
   );
@@ -86,36 +102,58 @@ const displayCountryArticles = (articles) => {
 
 const createArticleTemplate = (article) => {
   const articleItem = document.createElement("article");
+  articleItem.classList.add("articles__item");
   articleItem.innerHTML = `
-  <h3>${article.title}</h3>
-  <span>${article.publishedDateTime}</span>
-  <p>${article.excerpt}</p>
-  <a href="${article.webUrl}">Read more</a>
+  <a class="articles__itemTitleWrapper" href="${article.webUrl}">
+    <h3 class="articles__itemTitle">${article.title}</h3>
+  </a> 
+  <span class="articles__itemDate">${transformDate(
+    article.publishedDateTime
+  )}</span>
+  <p class="articles__itemTeaser">${article.excerpt}</p>
+  <a class="articles__itemLink" href="${article.webUrl}">Learn more</a>
+  <a class="articles__itemProvider" href="${article.provider.domain}">${
+    article.provider.domain
+  }</a>
 `;
   return articleItem;
+};
+
+const transformDate = (initialDate) => {
+  const date = `${initialDate.slice(0, 10)} ${initialDate.slice(11, 16)}`;
+  return date;
 };
 
 const createStatsTemplate = (infos) => {
   const info = `
   <div class="dataContainer">
     <div class="dataContainer__dataWrapper">
-      <p class="dataContainer__data dataContainer__data--date">${infos.lastUpdate}</p>
-      <span>Last update</span>
-      </div>
+      <p class="dataContainer__data dataContainer__data--date">${transformDate(
+        infos.lastUpdate
+      )}</p>
+    </div>
     <div class="dataContainer__dataWrapper">
-      <p class="dataContainer__data dataContainer__data--confirmed">${infos.confirmed}</p>
+      <p class="dataContainer__data dataContainer__data--confirmed">${
+        infos.confirmed
+      }</p>
       <span>Confirmed cases</span>
       </div>
     <div class="dataContainer__dataWrapper">
-      <p class="dataContainer__data dataContainer__data--recovered">${infos.recovered}</p>
+      <p class="dataContainer__data dataContainer__data--recovered">${
+        infos.recovered
+      }</p>
       <span> Recovered</span>
       </div>
     <div class="dataContainer__dataWrapper">
-      <p class="dataContainer__data dataContainer__data--critical">${infos.critical}</p>
+      <p class="dataContainer__data dataContainer__data--critical">${
+        infos.critical
+      }</p>
       <span>Critical cases</span>
       </div>
     <div class="dataContainer__dataWrapper">
-      <p class="dataContainer__data dataContainer__data--deaths">${infos.deaths}</p>
+      <p class="dataContainer__data dataContainer__data--deaths">${
+        infos.deaths
+      }</p>
       <span>Deaths</span>
     </div>
   </div>
