d26571e97dcf522c46923a44be002bc9c70615f0
WPaulos <wendypls.pro@gmail.com>
2020-05-27 23:51:31 +0200

    fetch

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+headers = new Headers({
+  Authorization: "Bearer IdwS1_wg3L3aTbw-q05t",
+});
+
+fetch(
+  "https://the-one-api.herokuapp.com/v1/character/5cd99d4bde30eff6ebccfdf3",
+  { headers }
+)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
