1a79c0a470597e044bae6eebd31f2eeef52e149d
cpetillot <constance.petillot@hetic.net>
2020-05-27 13:49:25 +0200

    trying to fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,19 @@
-const API_URL = "https://api.jokes.one/jod?";
 const $jokeButton = document.getElementById("jokeButton");
 const $joke = document.getElementById("joke");
 
 // Fetch random fact from API
 function getJoke() {
-  $fact.textContent = "Loading";
-  return fetch(`${API_URL}`)
+  $joke.textContent = "Loading";
+  return fetch(`https://api.jokes.one/jod?category=animal`)
     .then((res) => res.json())
-    .then((fact) => showFact(fact));
-
-  var myHeaders = new Headers();
-  myHeaders.append("GET", "https://api.jokes.one/jod?category=animal", true);
-  myHeaders.append("Content-type", application / json);
-  myHeaders.append("X-JokesOne-Api-Secret", "Your API here");
-
-  console.log(getJoke);
+    .then((joke) => showJoke(joke));
 }
 
 // Display joke into <p> tag
 function showJoke(joke) {
   $jokeContainer.textContent = "";
   const $JokeFragment = document.createDocumentFragment();
-  facts.forEach((joke) => $jokeFragment.appendChild(getJokeElement(joke)));
+  joke.forEach((joke) => $jokeFragment.appendChild(getJokeElement(joke)));
   $jokeContainer.appendChild($jokeFragment);
 }
 
@@ -29,3 +21,8 @@ function showJoke(joke) {
 function toggleButton($button) {
   $button.disabled = !$button.disabled;
 }
+
+$jokeButton.addEventListener("click", () => {
+  toggleButton($jokeButton);
+  getJoke().then(() => toggleButton($jokeButton));
+});
