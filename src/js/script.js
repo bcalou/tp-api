99c8912217b2c28c0d09db3f7a6233cc14bfbd79
Raphael <raphael.ferreira@hetic.net>
2020-05-27 11:46:10 +0200

    change request to get more movies

--- src/js/script.js
+++ src/js/script.js
@@ -4,68 +4,62 @@ const API_KEY = "12976118";
 const $movieTitleInput = document.getElementById("movie");
 const $movieForm = document.querySelector(".infos__form");
 const $notFoundArticle = document.querySelector(".not-found");
+const $movies = document.querySelector(".movies");
 
 // Return the input value
 function getMovieTitle(input) {
   return input.value;
 }
 
-// get the movie infos w/ API
-function getMovieInfos(movie) {
+// GET request to get movie list from title
+function getMovies(movieTitle) {
   axios
-    .get(`${API_URL}/?apikey=${API_KEY}&t=${movie}`)
+    .get(`${API_URL}/?apikey=${API_KEY}&s=${movieTitle}`)
     .then((res) => {
-      console.log(res.data.Response);
-
-      if (res.data.Response === "False") {
-        $notFoundArticle.classList.add("not-found--visible");
-      } else {
-        $notFoundArticle.classList.remove("not-found--visible");
-
-        editArticle(res.data);
-      }
+      console.log(res);
+      clearMovies($movies);
+      getMoviesData(res.data.Search);
     })
     .then((error) => {
       console.log(error);
     });
 }
 
-// fill the article with request infos
-function editArticle(infos) {
-  const $movieArticle = document.querySelector(".movie");
+// get the data for each movie
+function getMoviesData(movieList) {
+  movieList.forEach((movie) => {
+    createArticle(movie);
+  });
+}
 
-  $movieArticle.innerHTML = "";
+// remove the content from the container
+function clearMovies(container) {
+  container.innerHTML = "";
+}
+
+// Create an article with the movie's infos
+function createArticle(movie) {
+  const article = document.createElement("article");
+
+  article.classList.add("movie");
   createArticleElement({
     type: "h2",
-    content: infos.Title,
+    content: movie.Title,
     cssClass: "movie__title",
-    parent: $movieArticle,
+    parent: article,
   });
   createArticleElement({
     type: "h3",
-    content: `${infos.Runtime} - ${infos.Released}`,
+    content: `${movie.Year} - ${movie.Type}`,
     cssClass: "movie__sub-infos",
-    parent: $movieArticle,
+    parent: article,
   });
-  createArticleImage(infos.Poster, $movieArticle);
-  createArticleElement({
-    type: "p",
-    content: infos.Plot,
-    cssClass: "movie__plot",
-    parent: $movieArticle,
-  });
-  createArticleElement({
-    type: "p",
-    content: `Actors: ${infos.Actors}`,
-    cssClass: "movie__actors",
-    parent: $movieArticle,
-  });
-  createArticleElement({
-    type: "p",
-    content: `IMDb Rating: ${infos.imdbRating}`,
-    cssClass: "movie__rating",
-    parent: $movieArticle,
-  });
+
+  if (movie.Poster !== "N/A") {
+    createArticleImage(movie.Poster, article);
+  }
+
+  $movies.appendChild(article);
 }
 
 // create element w/ content and class in a parent
@@ -85,8 +79,66 @@ function createArticleImage(src, parent) {
   image.classList.add("movie__poster");
   parent.appendChild(image);
 }
-
 $movieForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  getMovieInfos(getMovieTitle($movieTitleInput));
+  getMovies(getMovieTitle($movieTitleInput));
+  // getMovieInfos(getMovieTitle($movieTitleInput));
 });
+
+// // get the movie infos w/ API
+// function getMovieInfos(movie) {
+//   axios
+//     .get(`${API_URL}/?apikey=${API_KEY}&t=${movie}`)
+//     .then((res) => {
+//       console.log(res.data.Response);
+
+//       if (res.data.Response === "False") {
+//         $notFoundArticle.classList.add("not-found--visible");
+//       } else {
+//         $notFoundArticle.classList.remove("not-found--visible");
+
+//         editArticle(res.data);
+//       }
+//     })
+//     .then((error) => {
+//       console.log(error);
+//     });
+// }
+
+// // fill the article with request infos
+// function editArticle(infos) {
+//   const $movieArticle = document.querySelector(".movie");
+
+//   $movieArticle.innerHTML = "";
+//   createArticleElement({
+//     type: "h2",
+//     content: infos.Title,
+//     cssClass: "movie__title",
+//     parent: $movieArticle,
+//   });
+//   createArticleElement({
+//     type: "h3",
+//     content: `${infos.Runtime} - ${infos.Released}`,
+//     cssClass: "movie__sub-infos",
+//     parent: $movieArticle,
+//   });
+//   createArticleImage(infos.Poster, $movieArticle);
+//   createArticleElement({
+//     type: "p",
+//     content: infos.Plot,
+//     cssClass: "movie__plot",
+//     parent: $movieArticle,
+//   });
+//   createArticleElement({
+//     type: "p",
+//     content: `Actors: ${infos.Actors}`,
+//     cssClass: "movie__actors",
+//     parent: $movieArticle,
+//   });
+//   createArticleElement({
+//     type: "p",
+//     content: `IMDb Rating: ${infos.imdbRating}`,
+//     cssClass: "movie__rating",
+//     parent: $movieArticle,
+//   });
+// }
