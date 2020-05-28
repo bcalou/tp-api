f2a9c2eafa980d2416d566363b0c96cc718c6e0c
Lucas <lucas.roche@hetic.net>
2020-05-28 15:45:31 +0200

    Started js script

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+
+
+const Discover = document.querySelector(".discover");
+fetch("https://api.nasa.gov/planetary/apod?api_key=hNbh4bjOeNIfhirPFzKPjM6WlMA4pLT4rQeAk1u0")
+    .then((res) => res.json())
+    .then((apod) => console.log(apod));
+
+
+    function getFacts() {
+        $factsContainer.textContent = 'Chargement en cours';
+      
+        return fetch(getFactsUrl())
+          .then(res => res.json())
+          .then(facts => showFacts(facts));
+      }
+
