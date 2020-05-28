6c6e0c37da644f15766f852e9777e8772cbfb1db
Michael vieira <mike@macbook-pro-de-michael.home>
2020-05-28 12:36:19 +0200

    add random request

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ const $formButton = document.getElementById('formButton');
 const $form = document.getElementById('formBreeds');
 
 
-
+//by breeds
 function getPhotos() {
   $photosContainer.textContent = "chargement de l'image";
 
@@ -22,36 +22,45 @@ function getPhotosUrl() {
   return `${API_URL}/breed/${getSelectedBreeds()}/images/random`;
 }
 
+//random 
+function getPhotosUrlRandom() {
+  return `${API_URL}/breeds/image/random`;
+}
+function getPhotosRandom() {
+  $photosContainer.textContent = "chargement de l'image";
 
-function showPhotos($photo) {
-  $photosContainer.appendChild(getPhotoElement($photo));
+  return fetch(getPhotosUrlRandom())
+  .then(res => res.json())
+  .then(photo => showPhotos(photo));
+}
+
+
+function showPhotos(photo) {
+  $photosContainer.appendChild(getPhotoElement());
   return getPhotoElement();
 
 }
 
-function getPhotoElement(getPhotosUrl) {
-  //const $photo = createElement({type: 'article'});
-    
-  //createElement({type: 'img', img: photo.text, parent: $photo});
-  //const $img = createElement({ type: 'img', parent: $photo });
-  //$img.src = photo.message;
-    
-  return $img;
+
+function getPhotoElement(photo) {
+  const $img = createElement({ type: 'img', parent: $photo });
+  $img.src = photo.message;
+
+  return $img
 }
 
 
 
 function createElement(photo) {
-  //const $img = createElement({ type: 'img', parent: $photo });
   
-  if (photo.img) {
+  console.log("ça marche !")
+  if (photo.parent) {
     $photo.textContent = photo.message;  
   }
   
   if (photo.parent) {
     options.parent.appendChild($element);
   }
-  
   return $photo;
 }
 
@@ -70,16 +79,20 @@ $showButton.addEventListener('click', () => {
   getPhotos().then(() => toggleButton($showButton)); 
 });
 
+$randomButton.addEventListener('click',() => {
+  toggleButton($randomButton);
+  getPhotosRandom().then(() => toggleButton($randomButton));
+});
 
 
 $formButton.addEventListener('click', () => {
   if(getComputedStyle($form).display != "none"){
     $form.style.display = "none";
-    $formButton.innerHTML = "afficher les races";
+    $formButton.innerHTML = "Afficher les races";
     $showButton.style.display="none";
   } else {
     $form.style.display = "block";
-    $formButton.innerHTML = "masquer les races";
+    $formButton.innerHTML = "Masquer les races";
     $showButton.style.display="block";
   }
 })
