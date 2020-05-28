9c15eb5fd7092bb7203abd3d1c66de7d2e24fbe5
annie.trn <annietth@users.noreply.github.com>
2020-05-28 19:35:11 +0200

    image add

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,19 @@
 console.log('Hello world');
-const apiUrl="https://pokeapi.co/api/v2";
-
-fetch(`${apiUrl}/pokemon/`)
-.then(Response=>console.log(Response.json())) 
+const apiUrl="https://api.thedogapi.com/v1/images/search?limit=5&page=10&order=Desc";
+const element = document.getElementById("container");
+const press = document.getElementById('press');
+function call() {
+    fetch(`${apiUrl}/breeds/`)
+    .then((res) => res.json())
+    .then((data) => showDogs(data))
+    .catch((err) => {
+    console.log(err);
+});
+}
+function showDogs(dog) {
+    element.innerHTML = 
+    dog.map(el=>{
+        return `<img src="${el.url}"/>`
+    });
+}
+press.addEventListener('click', call)
