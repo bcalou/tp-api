83d15c32735115fd27ae50506f3c9a1452848581
T69M <thomas.monmousseau@hetic.net>
2020-05-28 16:35:06 +0200

    almost finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const jokeForm = document.getElementById("jokeForm");
+const jokeText = document.getElementById("jokeText");
+
+jokeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("clicked the button");
+  const url = "https://api.icndb.com/jokes/random";
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.value.joke);
+      jokeText.innerHTML = data.value.joke;
+    });
+});
