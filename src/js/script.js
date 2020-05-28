f261305ff4e0cedf04bf5724e4967bee070055b1
Amy-Dev <emmy.peteytas@hetic.net>
2020-05-28 19:53:46 +0200

    I'm doing my best

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const $container= document.getElementById("Container")
 const $button = document.querySelector(".chuck__button")
 
 
+
 function getjokes(){
     $container.textContent= 'Chargement...'
 return fetch(`${apiurl}`)
@@ -33,8 +34,26 @@ getjokes()
      showjokes()
  })
 
+// Set counter default to zero
+var counter = 0
+
+// Display total
+$("#counter").text(counter);
+
+// When button is clicked
+$("#add").click(function(){
+  //Add 10 to counter
+  counter = counter + 1;
+	// Display total
+	$("#counter").text(counter);
+});
 
 
+//Subtract
+$("#subtract").click(function(){
+  counter = counter - 1;
+  $("#counter").text(counter);
+});
 
 
   /*{
