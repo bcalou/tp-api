bc6c80a7c695b76b8d835c27071ff7871014ee6e
gravity-zero <gravity.neo@gmail.com>
2020-05-28 16:02:03 +0200

    finish?

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,66 @@
 /**Doc API:
  * Everything : ?q=word + &language=
  */
-const $newsButton = document.getElementById("newsButton").addEventListener("click", (e) => {
+const placeholderimage = require("../asset/media/no-image-available.jpg");
+
+document.getElementById("form-search").addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log('lol', $newsButton)
-	var searchs = document.getElementsByClassName("search")[0].value;
-  const placeholderimage = require("../asset/media/no-image-available.jpg");
-  const apiEveryThing = "http://newsapi.org/v2/everything";
-  const sort = "popularity";
-  const lang = "fr";
-  var requestSearch = encodeURI(searchs);
-  const apiKey = "apiKey=d182478c672e466298066e128a76e0bd";
-	const $articleContainer = document.getElementsByClassName("articleContainer")[0];
+	const API_EVERYTHING = "http://newsapi.org/v2/everything";
 
-  fetch(`${apiEveryThing}?q=${requestSearch}&language=${lang}&sortBy=${sort}&pageSize=10&page=1&${apiKey}`)
-    .then((data) => data.json())
-    .then((response) => {
-      console.log(response);
-      let status = response.status;
-      var articles = status ? response.articles : noContent();
+	const params = {
+		sortBy: "popularity",
+		language: "fr",
+		apiKey: "d182478c672e466298066e128a76e0bd",
+		pageSize: 10,
+		page: 1,
+		q: encodeURI(document.querySelector(".search").value),
+	};
 
-        //Save value for each article
-        articles.forEach((response) => {
-          var images = (response.urlToImage ? image = response.urlToImage : image = placeholderimage);
-          var sources = response.source.name;
-          var titles = response.title;
-          var descriptions = response.description;
-          var urls = response.url;
+	const paramsObject = new URLSearchParams();
 
-        //Create container and html element for each article
-          var article = $articleContainer.appendChild(document.createElement("article"));
-          var link = article.appendChild(document.createElement("a"));
-          var hOne = article.appendChild(document.createElement("h1"));
-          var img = article.appendChild(document.createElement("img"));
-          var description = article.appendChild(document.createElement("ul"));
-          var articleContent = description.appendChild(document.createElement("li"));
-          var newspaper = description.appendChild(document.createElement("li"));
+	for (key in params) {
+		paramsObject.append(key, params[key]);
+	}
 
-          //Check if we have a title
-          if (titles !== "undefined") {
-            link.setAttribute = ('href', `"${urls}"`);
-            img.setAttribute = ('src', `"${images}"`);
-            link.href = `${urls}`;
-            hOne.textContent += `${titles}`;
-            img.src = `${image}`;
-            articleContent.textContent += `${descriptions}`;
-            newspaper.textContent += `${sources}`;
-            link.appendChild(hOne);
-            link.appendChild(img);
-            }
-        });
-    });
+	fetch(`${API_EVERYTHING}?${paramsObject.toString()}`)
+		.then((data) => data.json())
+		.then((response) => {
+			console.log(response);
+			let status = response.status;
+			var articles = status ? response.articles : noContent();
+
+			//Save value for each article
+			articles.forEach((response) => {
+				document
+					.querySelector(".articleContainer")
+					.appendChild(renderArticle(response));
+			});
+		});
 });
 
-function noContent(){
-  hOne.textContent += `"Articles correspondants : 0"`;
+function noContent() {
+	hOne.textContent += "Articles correspondants : 0";
+	return [];
 }
+
+const renderArticle = ({ title, source, description, url, urlToImage }) => {
+	var image = urlToImage || placeholderimage;
+	var article = document.createElement("article");
+	var link = article.appendChild(document.createElement("a"));
+	var hOne = article.appendChild(document.createElement("h1"));
+	var img = article.appendChild(document.createElement("img"));
+	var $description = article.appendChild(document.createElement("ul"));
+	var articleContent = $description.appendChild(document.createElement("li"));
+	var newspaper = $description.appendChild(document.createElement("li"));
+
+	link.setAttribute("href", url);
+	img.setAttribute("src", image);
+	link.href = url;
+	hOne.textContent += title;
+	img.src = image;
+	articleContent.textContent += description;
+	newspaper.textContent += source.name;
+	link.appendChild(hOne);
+	link.appendChild(img);
+	return article;
+};
