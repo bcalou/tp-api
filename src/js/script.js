a315ae6e95b6514b6109ed1e10f9b407e075be18
AchabHanna <hanna.achab@hetic.net>
2020-05-28 21:41:21 +0200

    adding scss

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,6 @@ const API_URL = "https://sv443.net/jokeapi/v2/joke/";
 const $factButton = document.getElementById("factButton");
 const $factsContainer = document.getElementById("factsContainer");
 
-// $factButton.addEventListener("click", () => {
-//   fetch(`${API_URL}?type=single`)
-//     .then(res => res.json())
-//     .then(res => ($factsContainer.textContent = res.joke));
-// });
-// $factButton.addEventListener("click", () => {
-//   fetch(`${API_URL}Any?blacklistFlags=${value}`)
-//     .then(res => res.json())
-//     .then(res => ($factsContainer.textContent = res.joke));
-//   let blackListValue = document.querySelectorAll(
-//     'input[name="blackList"]:checked'
-//   );
-//   blackListValue.forEach(element => {
-//     let value = element.value;
-//   });
-// });
-
 function getSelectedFlag() {
   document
     .querySelectorAll('input[name="blackList"]:checked')
