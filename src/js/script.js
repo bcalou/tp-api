0a0d1ccd9319dc673c761a55689486b6200b1321
Ramtin Attar <ramtinattar13@gmail.com>
2020-05-28 19:28:21 +0200

    autre changement

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,27 @@ $callAPI.addEventListener("click", () => {
 function listBooks(item, index) {
   $showResult.innerHTML += (index += 1) + "- " + item.name + "<br>";
 }
+
+// ----------------
+
+const $callAPI2 = document.getElementById("call2");
+const $showResult2 = document.getElementById("result2");
+
+headers = new Headers({
+  Authorization: "Bearer HDDZurhwaoc4RKzZ29sg",
+});
+
+$callAPI2.addEventListener("click", () => {
+  $showResult2.innerHTML = "Loading...";
+  return fetch("https://the-one-api.herokuapp.com/v1/movie", { headers })
+    .then((res) => res.json())
+    .then((movies) => {
+      console.log(movies);
+      $showResult2.innerHTML = "";
+      var $movieList = movies.docs;
+      $movieList.forEach(listMovies);
+    });
+});
+function listMovies(item, index) {
+  $showResult2.innerHTML += (index += 1) + "- " + item.name + "<br>";
+}
