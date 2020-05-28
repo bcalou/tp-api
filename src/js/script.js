79259bc3d4794673f859cd2b67a3ca12a590fd42
Lucas <lucas@lucas.home>
2020-05-28 19:32:15 +0200

    api

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+const jokeForm = document.getElementById("jokeForm");
+const jokeText = document.getElementById("jokeText");
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+jokeForm.addEventListener("submit", e => {
+  e.preventDefault();
+  getNames(e.target);
+});
+
+function getJokes(firstName, lastName) {
+  fetch(
+    `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
+  )
+    .then(res => res.json())
+    .then(data => seeJoke(e))
+    .catch(error => console.log(error));
+}
+function getNames(form) {
+  const firstName = form.querySelector("#firstName").value;
+  const lastName = form.querySelector("#lastName").value;
+  getJokes(firstName, lastName);
+}
+function seeJoke(data) {
+  jokeText.innerText = data.value.joke;
+}
